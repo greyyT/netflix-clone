4972b5bd86bca247180e003540b172fd82478900
greyyT <nhannguyen8338@gmail.com>
2023-08-09 16:42:59 +0700

    Add Billboard page with video preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import Billboard from '@/components/Billboard';
+import Navbar from '@/components/Navbar';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,12 +25,9 @@ export default function Home() {
   const { data: user } = useCurrentUser();
 
   return (
-    <div className="">
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as: {user?.email}</p>
-      <button onClick={() => signOut()} className="h-10 w-full bg-white">
-        Log Out!
-      </button>
-    </div>
+    <>
+      <Navbar />
+      <Billboard />
+    </>
   );
 }
